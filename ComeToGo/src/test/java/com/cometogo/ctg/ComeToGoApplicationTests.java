0eb5f9c0a68dc995d07a6b66fbcea203dd75c10b
Joonsoo <shail80200@kakao.com>
2025-09-03 16:53:14 +0900

    프로젝트 초기 세팅: Spring Boot 프로젝트 구조 및 기본 파일 추가 (Spring Security 작업 X)

---
+++ ComeToGo/src/test/java/com/cometogo/ctg/ComeToGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cometogo.ctg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeToGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
